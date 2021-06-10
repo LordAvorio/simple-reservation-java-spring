817613a4f9671f7aeb820a19fcc40c060caec671
LordAvorio <gadingramjets@gmail.com>
2021-06-11 00:36:29 +0700

    Preparing Project

---
+++ simple-reservation-gading-api/src/test/java/com/domain/simplereservationgadingapi/SimpleReservationGadingApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.domain.simplereservationgadingapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleReservationGadingApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
