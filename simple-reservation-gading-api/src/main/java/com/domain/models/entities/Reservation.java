0c15a523825674fa349cf0c8d6fca3e5789974fd
LordAvorio <gadingramjets@gmail.com>
2021-06-11 23:54:17 +0700

    Update API

--- simple-reservation-gading-api/src/main/java/com/domain/models/entities/Reservation.java
+++ simple-reservation-gading-api/src/main/java/com/domain/models/entities/Reservation.java
@@ -9,7 +9,6 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
 import javax.validation.constraints.Email;
-import javax.validation.constraints.Max;
 import javax.validation.constraints.NotEmpty;
 
 @Entity
